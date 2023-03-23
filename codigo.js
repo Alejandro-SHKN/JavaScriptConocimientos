cbd17d449d4fe46c9e536427e8563c617da46628
Alejandro <alejandroshkn@gmail.com>
2023-03-22 23:07:23 -0600

    Se comienza la seccion de Operadores Aritmeticos

--- codigo.js
+++ codigo.js
@@ -1,5 +1,18 @@
-let nombre = prompt("Dime tu nombre");
-alert(`Muy bien ${nombre} por favor, introduce tu edad a continuación`);
+// let nombre = prompt("Dime tu nombre");
 
-let edad = prompt(`Introduce tu edad`);
-alert(`De acuerdo ${nombre}, tu edad es: ${edad}`);
+
+// while (nombre == ""){
+//     alert(`Por favor introduce tu nombre`)
+//     nombre = prompt("Dime tu nombre");
+// }
+// alert(`Muy bien ${nombre}, por favor introduce tu edad a continuación`);
+// let edad = prompt(`Introduce tu edad`);
+// while (edad == ""){
+//     alert(`Por favor introduce tu edad`)
+//     edad = prompt("Introduce tu edad");
+// }
+// alert(`De acuerdo ${nombre}, tu edad es de ${edad} años`);
+
+
+//Operadores Aritméticos
+            
