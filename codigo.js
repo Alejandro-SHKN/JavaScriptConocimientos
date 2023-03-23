49bdccfc43251eed67417968069b2de19e5c9a38
Alejandro <alejandroshkn@gmail.com>
2023-03-22 22:16:58 -0600

    uso de let y prompt en html

---
+++ codigo.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Dime tu nombre");
+alert(`Muy bien ${nombre} por favor, introduce tu edad a continuación`);
+
+let edad = prompt(`Introduce tu edad`);
+alert(`De acuerdo ${nombre}, tu edad es: ${edad}`);
