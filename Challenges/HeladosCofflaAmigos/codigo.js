b6e67ee1f2601237037643db56b8878ee8577ec0
Alejandro <alejandroshkn@gmail.com>
2023-03-23 15:56:54 -0600

    Se agregó la carpeta de challenges y el codigo resuelto del primer reto

---
+++ Challenges/HeladosCofflaAmigos/codigo.js
@@ -0,0 +1,90 @@
+//1.54
+/*
+Roberto Tiene 1.5 USD
+Pedro Tiene 1.7 USD
+Coffla Tiene 3 USD
+
+Los precios de los helados son los siguentes:
+Palito de helado de agua: $0.6 USD
+Palito de helado de crema: $1 USD
+Bombón de Helado marca heladix: $1.6 USD
+Bombón de Helado marca heladovich: $1.7 USD
+Bombón de Helado marca helardo: $1.8 USD 
+Potecito de Helado con confites: $ 2.9 USD
+Pote de 1/4 KG -> $2.9 USD
+*/
+
+//Coffla
+let dineroCoffla = prompt(`Con cuanto dinero cuentas Coffla`);
+
+if (dineroCoffla >=0.6 && dineroCoffla<1) {
+    alert(`Coffla, comprate el Helado de agua`)
+}
+else if (dineroCoffla >=1 && dineroCoffla<1.6) {
+    alert(`Coffla, comprate el Helado de crema`)
+}
+else if (dineroCoffla >=1.6 && dineroCoffla<1.7) {
+    alert(`Coffla, comprate el Helado de marca Heladix`)
+}
+else if (dineroCoffla >=1.7 && dineroCoffla<1.8) {
+    alert(`Coffla, comprate el Helado de marca Heladovich`)
+}
+else if (dineroCoffla >=1.8 && dineroCoffla<2.9) {
+    alert(`Coffla, comprate el Helado de marca Heraldo`)
+}
+else if (dineroCoffla >=2.9) {
+    alert(`Coffla, comprate el potecito de helado con confites, o el pote de 1/4 KG`)
+}
+else{
+    alert(`Lo sentimos no cuentas con el dinero suficiente para poder comprar un helado`)
+}
+
+//Roberto
+let dineroRoberto = prompt(`Con cuanto dinero cuentas Roberto`);
+
+if (dineroRoberto >=0.6 && dineroRoberto<1) {
+    alert(`Roberto, comprate el Helado de agua`)
+}
+else if (dineroRoberto >=1 && dineroRoberto<1.6) {
+    alert(`Roberto, comprate el Helado de crema`)
+}
+else if (dineroRoberto >=1.6 && dineroRoberto<1.7) {
+    alert(`Roberto, comprate el Helado de marca Heladix`)
+}
+else if (dineroRoberto >=1.7 && dineroRoberto<1.8) {
+    alert(`Roberto, comprate el Helado de marca Heladovich`)
+}
+else if (dineroRoberto >=1.8 && dineroRoberto<2.9) {
+    alert(`Roberto, comprate el Helado de marca Heraldo`)
+}
+else if (dineroRoberto >=2.9) {
+    alert(`Roberto, comprate el potecito de helado con confites, o el pote de 1/4 KG`)
+}
+else{
+    alert(`Lo sentimos no cuentas con el dinero suficiente para poder comprar un helado`)
+}
+
+//Pedro
+let dineroPedro = prompt(`Con cuanto dinero cuentas Pedro`);
+
+if (dineroPedro >=0.6 && dineroPedro<1) {
+    alert(`Pedro, comprate el Helado de agua`)
+}
+else if (dineroPedro >=1 && dineroPedro<1.6) {
+    alert(`Pedro, comprate el Helado de crema`)
+}
+else if (dineroPedro >=1.6 && dineroPedro<1.7) {
+    alert(`Pedro, comprate el Helado de marca Heladix`)
+}
+else if (dineroPedro >=1.7 && dineroPedro<1.8) {
+    alert(`Pedro, comprate el Helado de marca Heladovich`)
+}
+else if (dineroPedro >=1.8 && dineroPedro<2.9) {
+    alert(`Pedro, comprate el Helado de marca Heraldo`)
+}
+else if (dineroPedro >=2.9) {
+    alert(`Pedro, comprate el potecito de helado con confites, o el pote de 1/4 KG`)
+}
+else{
+    alert(`Lo sentimos no cuentas con el dinero suficiente para poder comprar un helado`)
+}
